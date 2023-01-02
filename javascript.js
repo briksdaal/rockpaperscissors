fddabfb75a65c635a517bcb88db58d63cf850a61
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-01-02 20:29:55 +0200

    Add getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello rock paper scissors!");
+function getComputerChoice() {
+    let choiceNum = Math.floor(Math.random() * 3);
+    switch (choiceNum) {
+        case 0:
+            return "Rock";
+        break;
+        case 1:
+            return "Paper";
+        break;
+        case 2:
+            return "Scissors";
+        break;
+    }
+}
+
+console.log(getComputerChoice());
